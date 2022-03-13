d9fa5e3d67ec9bf3c9253782ea5514b131b4c855
anonymous anonymous <emi.rosanwo@kellogg.northwestern.edu>
2022-03-13 18:41:11 +0000

    Whitelist columns in Page for ActiveAdmin

--- app/admin/pages.rb
+++ app/admin/pages.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Page do
 
+ permit_params :image, :writer_name, :content
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
