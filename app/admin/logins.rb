799042f68497ae6ce3faf51ee41bd78ed19be01f
anonymous anonymous <emi.rosanwo@kellogg.northwestern.edu>
2022-03-13 18:41:19 +0000

    Whitelist columns in Login for ActiveAdmin

--- app/admin/logins.rb
+++ app/admin/logins.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Login do
 
+ permit_params :useremail, :usertype, :password, :reader_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
